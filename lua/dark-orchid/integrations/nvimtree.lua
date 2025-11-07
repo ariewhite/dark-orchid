534daad1ad100c9c43ebddff27181c5023cb43a1
Arie White <zetpos9@gmail.com>
2025-11-08 00:17:30 +0300

    symlink icon

--- lua/dark-orchid/integrations/nvimtree.lua
+++ lua/dark-orchid/integrations/nvimtree.lua
@@ -6,24 +6,24 @@ local M = {
   NvimTreeRootFolder       = { fg = palette.fuchsia_400, bold = true },
   NvimTreeOpenedFolderName = { fg = palette.purple_500, bold = true },
   NvimTreeEmptyFolderName  = { fg = palette.purple_500 },
-  NvimTreeFolderName       = { fg=palette.purple_500 },
+  NvimTreeFolderName       = { fg = palette.purple_500 },
   -- git
   NvimTreeGitDeleted       = { fg = palette.red_600 },
   NvimTreeGitDirty         = { fg = palette.yellow_600 },
   NvimTreeGitNew           = { fg = palette.green_800 },
   -- files 
   NvimTreeFileIcon         = { fg = palette.zinc_200 },
-  NvimTreeLuaIcon          = { fg = palette.zinc_200 },
   NvimTreeExecFile         = { fg = palette.white },
   NvimTreeOpenedFile       = { fg = palette.zinc_900, bold = true },
   NvimTreeSpecialFile      = { fg = palette.white, underline = true },
   NvimTreeSymlink          = { fg = palette.white },
-  NvimTreeImageFile    = { fg = palette.purple_500 },
+  NvimTreeSymlinkIcon      = { fg = palette.white },
+  NvimTreeImageFile        = { fg = palette.purple_500 },
   -- other
-  NvimTreeIndentMarker = { fg = palette.zinc_600 },
+  NvimTreeIndentMarker     = { fg = palette.zinc_600 },
 
-  NvimTreeNormal       = { fg = palette.zinc_200, bg = palette.none },
-  NvimTreeNormalNC     = { fg = palette.zinc_200, bg = palette.none },
+  NvimTreeNormal           = { fg = palette.zinc_200, bg = palette.none },
+  NvimTreeNormalNC         = { fg = palette.zinc_200, bg = palette.none },
 
   NvimTreeWinSeparator = {
     fg = palette.zinc_800,
