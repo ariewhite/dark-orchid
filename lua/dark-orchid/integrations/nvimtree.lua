01fab89313668184fb5dd2bf8cd356abbcbe8797
Чингиз Мурзабаев <zetpos9@gmail.com>
2025-11-07 16:28:53 +0300

    test2

--- lua/dark-orchid/integrations/nvimtree.lua
+++ lua/dark-orchid/integrations/nvimtree.lua
@@ -1,30 +1,30 @@
 local palette = require("dark-orchid.colors")
 
 local M = {
-    NvimTreeFolderIcon   = { bg = palette.none, fg = palette.purple_500 },
+  NvimTreeFolderIcon   = { bg = palette.none, fg = palette.purple_500 },
 
-    NvimTreeGitDeleted   = { fg = palette.red_600 },
-    NvimTreeGitDirty     = { fg = palette.yellow_600 },
-    NvimTreeGitNew       = { fg = palette.green_800 },
+  NvimTreeGitDeleted   = { fg = palette.red_600 },
+  NvimTreeGitDirty     = { fg = palette.yellow_600 },
+  NvimTreeGitNew       = { fg = palette.green_800 },
 
-    NvimTreeImageFile    = { fg = palette.purple_500 },
-    NvimTreeSpecialFile  = { fg = palette.fuchsia_300, underline = true },
+  NvimTreeImageFile    = { fg = palette.purple_500 },
+  NvimTreeSpecialFile  = { fg = palette.fuchsia_300, underline = true },
 
-    NvimTreeSymlink      = { fg = palette.blue_200 },
+  NvimTreeSymlink      = { fg = palette.blue_200 },
 
-    NvimTreeIndentMarker = { fg = palette.zinc_600 },
+  NvimTreeIndentMarker = { fg = palette.zinc_600 },
 
-    NvimTreeNormal       = { fg = palette.zinc_200, bg = palette.zinc_950 },
-    NvimTreeNormalNC     = { fg = palette.zinc_200, bg = palette.zinc_950 },
+  NvimTreeNormal       = { fg = palette.zinc_200, bg = palette.zinc_950 },
+  NvimTreeNormalNC     = { fg = palette.zinc_200, bg = palette.zinc_950 },
 
-    NvimTreeOpenedFile   = { bg = palette.zinc_900 },
+  NvimTreeOpenedFile   = { bg = palette.zinc_900 },
 
-    NvimTreeRootFolder   = { fg = palette.fuchsia_400, bold = true },
+  NvimTreeRootFolder   = { fg = palette.fuchsia_400, bold = true },
 
-    NvimTreeWinSeparator = {
-      fg = opts.styles.sidebars == "transparent" and palette.zinc_800 or palette.zinc_950,
-      bg = palette.zinc_950
-    },
+  NvimTreeWinSeparator = {
+    fg = palette.zinc_800,
+    bg = palette.zinc_950,
+  },
 }
 
 return M
